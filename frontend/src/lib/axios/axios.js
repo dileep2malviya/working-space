a178f54302ddae3152f62bcb6643c555348c1c2c
kk <dileep2malviya@gmail.com>
2026-09-21 01:09:04 +0530

    change url

--- frontend/src/lib/axios/axios.js
+++ frontend/src/lib/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://working-space-2.onrender.com/api/v1/";
+const baseURL = "https://working-space-3.onrender.com/api/v1/";
 
 const axiosInstance = axios.create({
   baseURL,
